7e349fcc709bfca47ab7e925876aa6114aafbdf6
Alexander Vandenesse <vandenessea96@gmail.com>
2021-07-17 19:58:20 -0400

    portfolio-react

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+//Below we are imoporting our portfolio file from our componets folder
+import Portfolio from './componets/Portfolio';
+import './App.css';
+
+
+// React apps typically have a single App component at the very top that can reference other React components.
+// This component, `App`, is our main component that is importing `portfolio` and rendering it in the return method.
+
+
+function App() {
+    return (
+      
+      <div className="portfolio-app">
+      <Portfolio />
+    </div>
+      
+    )
+  }
+
+
+export default App;
+
+
