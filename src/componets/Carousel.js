7771b6144e5fb6df1e35b4f3493eca72d5298035
Alexander Vandenesse <vandenessea96@gmail.com>
2021-07-18 21:35:52 -0400

    webpack installed

--- src/componets/Carousel.js
+++ src/componets/Carousel.js
@@ -2,6 +2,8 @@
 import React  from 'react';
 //import Carousel from './Carousel';
 import Carousel from 'react-bootstrap/Carousel'
+import Body from '../Images/Body.jpg'
+import Blue from '../Images/Blue.jpg'
 
 
 function carousel() {
@@ -9,21 +11,21 @@ function carousel() {
 return (
 
     <Carousel>
-    <Carousel.Item interval={1000}>
+    <Carousel.Item interval={9999}>
       <img
         className="d-block w-100"
-        src="holder.js/800x400?text=First slide&bg=373940"
-        alt="First slide"
+        src={Body}
+        alt="Body slide"
       />
       <Carousel.Caption>
         <h3>First slide label</h3>
         <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
       </Carousel.Caption>
     </Carousel.Item>
-    <Carousel.Item interval={500}>
+    <Carousel.Item interval={9999}>
       <img
         className="d-block w-100"
-        src="public\blue-light.jpg"
+        src={Blue}
         alt="Second slide"
       />
       <Carousel.Caption>
@@ -31,7 +33,7 @@ return (
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
       </Carousel.Caption>
     </Carousel.Item>
-    <Carousel.Item>
+    <Carousel.Item >
       <img
         className="d-block w-100"
         src="holder.js/800x400?text=Third slide&bg=20232a"
